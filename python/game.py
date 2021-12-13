08eccb696e64f8184af5d3d043b2066e2499e456
MMMatth <mat.loup04@gmail.com>
2021-12-13 16:48:25 +0100

    Premier truc joueur

--- python/game.py
+++ python/game.py
@@ -1,6 +1,7 @@
 import pygame
 import world
 import ClassPG
+import sys
 
 
 def gaming():
@@ -9,27 +10,34 @@ def gaming():
 
     d = {}
 
+    tile_size = 8
     sizemap = 100
 
     worlde = world.Terrain()
 
     map = worlde.GenerateTerrain()
-    print(map)
-    print(len(map))
 
+    print(map)
     tile = pygame.sprite.Group()
 
-    for row in map:
-        d[str(row[1])] = world.tiles(f"../img/tile/bloc/PNG/Tiles/tile_{str(row[1])}.png", row[0][0]*8+10,  row[0][1]*8+500)
-        tile.add(d[str(row[1])])
+    for column in map:
+        for row in column:
+            d[str(row[1])] = world.tiles(str(row[1]),f"../img/tile/bloc/PNG/Tiles/tile_{str(row[1])}.png", row[0][0]*tile_size,  row[0][1]*tile_size + 500, tile_size)
+
+            tile.add(d[str(row[1])])
 
     while game.running:
         game.gameloop()
 
         tile.draw(game.screen)
 
-        game.eventpy()
-        
-        pygame.display.update()
+        for event in pygame.event.get():  # parcours de tous les event pygame dans cette fenêtre
 
+            if event.type == pygame.QUIT:  # si l'événement est le clic sur la fermeture de la fenêtre
+                sys.exit()
+            x, y = pygame.mouse.get_pos()
+            for i in tile:
+                if i.rect.collidepoint(x, y):
+                    print(i)
+        pygame.display.update()
 gaming()
--- python/world.py
+++ python/world.py
@@ -1,22 +1,21 @@
 import pygame, sys, os, random, noise
-from pygame.locals import *
-from perlin_noise import PerlinNoise
 
 
 class tiles(pygame.sprite.Sprite):
-	def __init__(self, image, x, y):
+	def __init__(self, name, image, x, y, size=16):
 		super().__init__()
+		self.name = name
 		self.image = pygame.image.load(image).convert_alpha()
-		self.size = 8
+		self.size = size
 		self.image = pygame.transform.scale(self.image, (self.size, self.size))
 		self.rect = self.image.get_rect()
 		self.rect.x, self.rect.y = x, y
 
-	def iblit(self, screen):
-		screen.blit(self.image, self.rect)
+	def getcoords(self):
+		return (round(self.rect.x / self.size), round(self.rect.y / self.size))
 
-	'''def __repr__(self):
-		return str([self.rect.x, self.rect.y])'''
+	def __repr__(self):
+		return self.name
 
 
 class Terrain:
@@ -24,9 +23,10 @@ class Terrain:
 
 		self.dirtlayerheight = 5
 		self.surfaceValue = -0.225
+		self.basevalue = 0
 
 		self.generateCave = True
-		self.seed = random.randint(-10000, 10000)
+		self.seed = 5 #random.randint(-10000, 10000)
 		self.worldsize = 150
 		self.CaveFreq = 0.08
 		self.TerrainFreq = 0.02
@@ -37,7 +37,8 @@ class Terrain:
 
 		self.width = 1200
 		self.height = 800
-		self.map = []
+		self.ligne = []
+		self.map = [self.ligne]*self.worldsize
 
 
 	def generate(self):
@@ -50,7 +51,6 @@ class Terrain:
 			for j in range(self.worldsize):
 				noise_val = noise.pnoise2((i + self.seed) * self.CaveFreq, (j + self.seed) * self.CaveFreq, 2, 0.5, 2,
 										  1024, 1024)
-				print(noise_val)
 				row.append(noise_val)
 			pic.append(row)
 		print('Gen noise finished')
@@ -59,51 +59,69 @@ class Terrain:
 	def GenerateTerrain(self):
 		x = 0
 		noiseeffet = self.GenerateNoiseTexture()
-		while x < self.worldsize:
+		while x < self.worldsize :
 			y = 0
 			height = noise.pnoise2((x + self.seed) * self.TerrainFreq, self.seed * self.TerrainFreq, 2, 0.5, 2, 1024,
 								   1024) * self.heightmultiplier + self.heightaddtion
-			while y < height:
+			while y < self.worldsize:
 				if (y < height - self.dirtlayerheight + random.randint(-1, 1)):
 					element = 'stone'
 				elif y < height - 1:
 					element = 'dirt'
-				else:
-					# Generation herbe
+				elif y < height:
 					element = 'grass'
+				else:
+					element = 'air'
+
 				if self.generateCave:
 					if noiseeffet[x][y] > self.surfaceValue:
 						self.PlaceTile(x, y, element)
+					else:
+						self.PlaceTile(x, y, 'air')
 				else:
 					self.PlaceTile(x, y, element)
 				y += 1
+
+			self.map[x] = self.ligne
+			self.ligne = []
 			x += 1
 
+		x = 0
 		while x < self.worldsize:
 			y = 0
 			height = noise.pnoise2((x + self.seed) * self.TerrainFreq, self.seed * self.TerrainFreq, 2, 0.5, 2, 1024,
 								   1024) * self.heightmultiplier + self.heightaddtion
+			self.ligne = self.map[x]
 			while y < height:
 				if (y < height - self.dirtlayerheight + random.randint(-1, 1)):
-					print('couche herbe')
+					pass
 				elif y < height - 1:
-					print('couche terre')
+					pass
 				else:
 					# Generation herbe
 					self.tree = random.randint(0, self.treechance)
 
-					if self.tree == 1:
+					if self.tree == 1 :
 						# Generation d'un arbre
-						self.GenerateTree(x, y + 1)
+						print(self.map[0][0][1])
+						if self.map[x][y][1] == 'grass':
+							self.GenerateTree(x, y + 1)
+
 				y += 1
+			self.map[x] = self.ligne
 			x += 1
 
 		print('Gen map Finished')
-
 		return self.map
 
 	def PlaceTile(self, x, y, element):
-		self.map += [[[x, -y], element]]
+		self.ligne += [[[x, -y], element]]
 
 	def GenerateTree(self, x, y):
-		return self.PlaceTile(x, y, 'trunk_bottom')
+		self.PlaceTile(x, y, 'trunk_bottom')
+		self.PlaceTile(x, y+1, 'trunk_mid')
+		self.PlaceTile(x, y+2, 'trunk_mid')
+		self.PlaceTile(x, y+3, 'trunk_side')
+		self.PlaceTile(x-1, y + 3, 'leaves')
+		self.PlaceTile(x , y + 4, 'leaves')
+		self.PlaceTile(x + 1, y + 3, 'leaves')
