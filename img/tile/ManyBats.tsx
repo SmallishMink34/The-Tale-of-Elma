84db431692bc62338d0ebcf7dfc06f3a4b9bb605
SmallIshmink <mattheo.m10@gmail.com>
2022-05-09 22:08:08 +0200

    Menu + Touche changeable + map + resolution bug

---
+++ img/tile/ManyBats.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="ManyBats" tilewidth="32" tileheight="32" tilecount="14758" columns="94">
+ <image source="32x32/tileset-pokemon_dawn.png" width="3008" height="5038"/>
+</tileset>
