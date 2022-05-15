eb2154b67d43dc3f517c23b037629b8fd5f05058
SmallIshmink <mattheo.m10@gmail.com>
2022-05-15 18:17:34 +0200

    Transition Screen, New Village Map,

---
+++ img/tile/Interior.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="Interior" tilewidth="32" tileheight="32" tilecount="1424" columns="16">
+ <image source="32x32/Interiors_free_32x32.png" width="512" height="2848"/>
+</tileset>
---
+++ img/tile/Library.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="Library" tilewidth="32" tileheight="32" tilecount="576" columns="24">
+ <image source="32x32/Meubles.png" width="768" height="768"/>
+</tileset>
