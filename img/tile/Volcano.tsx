204775595731db7fae9ed47316773ce21f2984ad
SmallIshmink <mattheo.m10@gmail.com>
2022-05-30 22:11:43 +0200

    Volcano Zone, bug, real-time Reload map,
    Took 12 seconds

---
+++ img/tile/Volcano.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="Volcano" tilewidth="32" tileheight="32" tilecount="840" columns="40">
+ <image source="32x32/ashlands_tileset.png" width="1280" height="672"/>
+</tileset>
